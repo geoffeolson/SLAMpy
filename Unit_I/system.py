8eda12303a2ca011535b91cb383bfd1feff8ee43
Geoff <geoffeolson@gmail.com>
2025-06-23 12:09:19 +0800

    system

--- Unit_I/system.py
+++ Unit_I/system.py
@@ -2,15 +2,16 @@ from lego_robot import *
 from math import sin, cos, pi, sqrt
 import numpy as np
 import os
-os.chdir("Unit_I")
+import json
 from slam_d_library import get_observations, write_cylinders
 from extended_kalman_filter import EKF
 from graph_slam import GraphSLAM
-import json
-
+from lego_robot import LegoLogfile
+from slam_d_library import get_observations
 
 class System:
     def __init__(self, ekf, graph_slam):
+
         # Dependencies
         self.ekf = ekf
         self.graph_slam = graph_slam
@@ -20,54 +21,29 @@ class System:
         self.covariances = []
         self.matched_ref_cylinders = []
 
-        # # Robot constants
-        # self.scanner_displacement = 30.0
-        # self.ticks_to_mm = 0.349
-        # self.robot_width = 155.0
-
-        # # Cylinder extraction and matching constants
-        # self.minimum_valid_distance = 20.0
-        # self.depth_jump = 100.0
-        # self.cylinder_offset = 90.0
-        # self.max_cylinder_distance = 300.0
-
-
-
-    def run(self):
-        from lego_robot import LegoLogfile
-        from slam_d_library import get_observations
-
-        # Read data
-        logfile = LegoLogfile()
-        logfile.read("robot4_motors.txt")
-        logfile.read("robot4_scan.txt")
-        logfile.read("robot_arena_landmarks.txt")
+    # Main System Loop
+    def run(self, logfile):
         reference_cylinders = [l[1:3] for l in logfile.landmarks]
-
         for i in range(len(logfile.motor_ticks)):
+
+            # Odometry
             control = np.array(logfile.motor_ticks[i]) * self.ekf.ticks_to_mm
             x_i, x_j, Sigma_ij = self.ekf.predict(control)
+            self.graph_slam.add_motion_constraint(x_j, Sigma_ij)
+            self.states.append(self.ekf.state)
+            self.covariances.append(self.ekf.covariance)
+
+            # Obervations
             observations = get_observations(
                 logfile.scan_data[i],
                 self.ekf.depth_jump, self.ekf.minimum_valid_distance, self.ekf.cylinder_offset,
                 self.ekf.state, self.ekf.scanner_displacement,
                 reference_cylinders, self.ekf.max_cylinder_distance)
-
-            # for j in range(len(observations)):
-            #     self.ekf.correct(*observations[j])
-
+            self.matched_ref_cylinders.append([m[1] for m in observations])
             for obs in observations:
                 pose_index, z_ik, landmark_index, Sigma_ik = obs
                 self.ekf.correct(pose_index, z_ik, landmark_index, Sigma_ik)
                 self.graph_slam.add_observation_constraint(pose_index, landmark_index, z_ik, Sigma_ik)
-
- 
-
-            self.states.append(self.ekf.state)
-            self.covariances.append(self.ekf.covariance)
-            self.matched_ref_cylinders.append([m[1] for m in observations])
-
-            self.graph_slam.add_motion_constraint(x_j, Sigma_ij)
 
     def write_ekf_results(self, filename="Results_EKF.txt"):
         from math import sqrt, cos, sin
@@ -90,9 +66,9 @@ class System:
                 f.write(f"F {x} {y} {θ}\n")
         print(f"Wrote {len(self.graph_slam.poses)} poses and constraints to {filename}")
 
+def lego_robot_test():
 
-
-if __name__ == '__main__':
+    #Initialize EKF
     config_json = """
     {
         "ticks_to_mm": 0.349,
@@ -108,46 +84,32 @@ if __name__ == '__main__':
         "measurement_angle_stddev": 0.2617993877991494
     }
     """
-
     config = json.loads(config_json)
     initial_state = np.array([1850.0, 1897.0, 213.0 / 180.0 * np.pi])
     initial_covariance = np.diag([100.0**2, 100.0**2, (10.0 / 180.0 * np.pi) ** 2])
-
     ekf = EKF(initial_state.copy(), initial_covariance, 0, 0, 0, 0, 0, 0)  # dummy values
     ekf.read_json(config)
 
+    #Initialize Graph SLAM
     graph_slam = GraphSLAM()
     graph_slam.add_pose(initial_state.copy())
+    
+    # Read Robot Data
+    logfile = LegoLogfile()
+    logfile.read("robot4_motors.txt")
+    logfile.read("robot4_scan.txt")
+    logfile.read("robot_arena_landmarks.txt")
 
+    # Run Main Sytyem
     system = System(ekf, graph_slam)
-    system.run()
-    system.write_ekf_results()
-    system.write_graph_slam_results()
+    system.run(logfile)
+    system.write_ekf_results("Results_EKF.txt")
+    system.write_graph_slam_results("Results_Graph_Slam.txt")
 
 
+if __name__ == '__main__':
+        # Get the directory of the current script
+    python_file_directory = os.path.dirname(os.path.abspath(__file__))
+    print("Current Directory " + os.chdir(python_file_directory))
+    lego_robot_test()
 
-# if __name__ == '__main__':
-#     from extended_kalman_filter import EKF
-#     from graph_slam import GraphSLAM
-#     import numpy as np
-
-#     # Constants
-#     scanner_displacement = 30.0
-#     initial_state = np.array([1850.0, 1897.0, 213.0 / 180.0 * np.pi])
-#     initial_covariance = np.diag([100.0**2, 100.0**2, (10.0 / 180.0 * np.pi) ** 2])
-
-#     # Create instances
-#     ekf = EKF(initial_state.copy(), initial_covariance,
-#               robot_width=155.0, scanner_displacement=scanner_displacement,
-#               control_motion_factor=0.35, control_turn_factor=0.6,
-#               measurement_distance_stddev=200.0,
-#               measurement_angle_stddev=15.0 / 180.0 * np.pi)
-
-#     graph_slam = GraphSLAM()
-#     graph_slam.add_pose(initial_state.copy())
-
-#     # Run system
-#     system = System(ekf, graph_slam)
-#     system.run()
-#     system.write_ekf_results()
-#     system.write_graph_slam_results()
